6ccc0ed7d2a13dbd200640b9323a0a57fdc8f776
vorvarinoo <n.styrman@yandex.ru>
2026-03-16 13:17:25 +0300

    прогнал через eslint

--- src/js/modules/configs.js
+++ src/js/modules/configs.js
@@ -25,26 +25,26 @@ const modalConfig = {
 
 const validateConfig = {
   justValidate: {
-    errorFieldCssClass: "is-invalid",
-    errorLabelCssClass: "error-massage",
+    errorFieldCssClass: 'is-invalid',
+    errorLabelCssClass: 'error-massage',
     errorLabelStyle: {
-      color: "var(--error)",
-      marginTop: "6px",
-      fontSize: "12px",
-      textAlign: "left",
+      color: 'var(--error)',
+      marginTop: '6px',
+      fontSize: '12px',
+      textAlign: 'left',
     },
-    errorFormClass: "shaked",
+    errorFormClass: 'shaked',
     errorTimeout: 1500,
   },
   errorTimeout: 1500,
   mask: {
-    bodyMask: " (___) ___ __ __",
+    bodyMask: ' (___) ___ __ __',
   },
 };
 
 const observerConfig = {
   scrollTop: {
-    rootMargin: "600px",
+    rootMargin: '600px',
     threshold: 1,
   },
 };
