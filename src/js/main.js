8366481d4916bb0cdb59ad63bc0fa604b8f64ae7
vorvarinoo <n.styrman@yandex.ru>
2026-03-16 13:16:24 +0300

    добавления табов

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,22 @@
-import './modules/libsCss.js';
-import 'focus-visible';
+import "./modules/libsCss.js";
+import "focus-visible";
 
-import { initSiteSettings } from './modules/settings.js';
+import { initSiteSettings } from "./modules/settings.js";
 
-import { initModals } from './modules/modal.js';
-import { initSliders } from './modules/slider.js';
-import { validateForms } from './modules/validate.js';
-import { initQuiz } from './modules/quiz.js';
+import { initModals } from "./modules/modal.js";
+import { initSliders } from "./modules/slider.js";
+import { validateForms } from "./modules/validate.js";
+import { initQuiz } from "./modules/quiz.js";
+import { initTabs } from "./modules/tabs.js";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   initSiteSettings();
-  initQuiz();
 
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     validateForms();
     initModals();
     initSliders();
+    initQuiz();
+    initTabs();
   });
 });
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,14 @@
+import "justtabs/dist/just-tabs.min.css";
+import JustTabs from "justtabs";
+
+const initTabs = () => {
+  new JustTabs("recipient-contacts", {
+    startTabIndex: 1,
+  });
+
+  new JustTabs("delivery-date", {
+    startTabIndex: 1,
+  });
+};
+
+export { initTabs };
