95682ecfbe5d9030c68c3af7734941ac6f3513b0
vorvarinoo <n.styrman@yandex.ru>
2026-03-18 17:34:18 +0300

    Обновить верстку квиза и стили
    - переработать секции квиза и price-line
    
    - обновить стили кнопок, таймлайна и типографики
    
    - скорректировать настройки и конфиги JS

--- src/js/modules/configs.js
+++ src/js/modules/configs.js
@@ -8,7 +8,7 @@ const sliderConfig = {
     },
     loop: true,
     effect: 'coverflow',
-    slidesPerView: 3,
+    slidesPerView: 'auto',
     centeredSlides: true,
     grabCursor: true,
     coverflowEffect: {
--- src/js/modules/settings.js
+++ src/js/modules/settings.js
@@ -1,27 +1,17 @@
 import baguetteBox from 'baguettebox.js';
 import SmoothScroll from 'smooth-scroll';
-import AcceptCookiePopup from 'accept-cookie-popup';
 
-import {
-  smoothScrollConfig
-} from './configs.js';
+import { smoothScrollConfig } from './configs.js';
 
-import {
-  iosVhFix
-} from './utils.js';
+import { iosVhFix } from './utils.js';
 
-import {
-  initScrollObserver,
-} from './scroll-observer.js';
+import { initScrollObserver } from './scroll-observer.js';
 
 const initSiteSettings = () => {
   iosVhFix();
   initScrollObserver();
-  new SmoothScroll( 'a[href*="#"]', smoothScrollConfig );
-  new AcceptCookiePopup();
-  [ '[data-gallery]' ].map( ( item ) => baguetteBox.run( item ) );
+  new SmoothScroll('a[href*="#"]', smoothScrollConfig);
+  ['[data-gallery]'].map((item) => baguetteBox.run(item));
 };
 
-export {
-  initSiteSettings,
-};
+export { initSiteSettings };
