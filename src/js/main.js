5195a99150f1d87367f1743f1ddc544820451a12
vorvarinoo <n.styrman@yandex.ru>
2026-03-31 10:46:45 +0300

    Удалить неиспользуемый модуль edit-section и его разметку

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,6 @@ import { validateForms } from './modules/validate.js';
 import { initQuiz } from './modules/quiz.js';
 import { initGetCertificateScreens } from './modules/quiz-get-certificate.js';
 import { initTabs } from './modules/tabs.js';
-import { initEditSection } from './modules/edit-section.js';
 import { initPriceLineDrag } from './modules/price-line-drag.js';
 import { initCertificatePrice } from './modules/certificate-price.js';
 import { initChoices } from './modules/choices.js';
@@ -30,7 +29,6 @@ document.addEventListener('DOMContentLoaded', () => {
     initQuiz();
     initGetCertificateScreens();
     initTabs();
-    initEditSection();
     initPriceLineDrag();
     initCertificatePrice();
     initChoices();
